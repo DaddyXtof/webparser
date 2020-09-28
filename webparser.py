96b2f0be764fec5050dda2d2917dc453d82df042
From Surface <christophe.dehon@gmail.com>
2020-09-28 09:57:10 +0100

    Added bs4 and print of links

--- webparser.py
+++ webparser.py
@@ -4,3 +4,6 @@ import bs4
 target = "http://onecloudstreet.com"
 site = requests.get(target)
 print(site.content)
+soup = bs4.BeautifulSoup(site.content, 'html.parser')
+for link in soup.find_all('a'):
+    print(link.get('href'))
