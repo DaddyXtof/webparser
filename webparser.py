87cbe27985cc80e7078b54b45e73335be0d8e7c3
From Surface <christophe.dehon@gmail.com>
2020-09-28 10:09:29 +0100

    Implemented def for recursion

--- webparser.py
+++ webparser.py
@@ -2,6 +2,14 @@ import requests
 import bs4
 import argparse
 
+def retrieveURL(url):
+    print("#### Retrieving: "+ url)
+    site = requests.get(target)
+    # print(site.content)
+    soup = bs4.BeautifulSoup(site.content, 'html.parser')
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+
 my_parser = argparse.ArgumentParser(description='Crawl URL and list all <a> links found')
 my_parser.add_argument('url',
                        metavar='url',
@@ -9,9 +17,5 @@ my_parser.add_argument('url',
                        help='the URL to crawl - for example: http://onecloudstreet.com')
 args = my_parser.parse_args()
 target = args.url
-site = requests.get(target)
-# print(site.content)
-soup = bs4.BeautifulSoup(site.content, 'html.parser')
-for link in soup.find_all('a'):
-    print(link.get('href'))
+retrieveURL(target)
 
