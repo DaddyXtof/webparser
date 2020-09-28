58458392aa07b42035e625a97ba22ed4136f9510
From Surface <christophe.dehon@gmail.com>
2020-09-28 13:27:41 +0100

    Minor updates

--- webparser.py
+++ webparser.py
@@ -3,18 +3,18 @@ import bs4
 import argparse
 import tldextract
 
-alreadyvisited = [] # list of urls that have already been crawled
+alreadyvisited = []  # list of urls that have already been crawled
 
 def retrieveURL(turl):
     print("#### Retrieving: "+ turl)
-    alreadyvisited.append(turl) # keeping track of urls already visited
-    currentpagelinks = [] # list of links on the page being parsed
+    alreadyvisited.append(turl)  # keeping track of urls already visited
+    currentpagelinks = []  # list of links on the page being parsed
     site = requests.get(turl)
     soup = bs4.BeautifulSoup(site.content, 'html.parser')
     for link in soup.find_all('a'):
         linkstring = link.get('href')
         if linkstring is not None:
-            if linkstring[0:4].lower() == 'http': # ignoring anchors etc...
+            if linkstring[0:4].lower() == 'http':  # ignoring anchors etc...
                 if args.internal and domain in linkstring:
                     print('\t' + linkstring)
                     currentpagelinks.append(linkstring)
@@ -22,7 +22,7 @@ def retrieveURL(turl):
                     print('\t' + linkstring)
                     currentpagelinks.append(linkstring)    
         
-    if currentpagelinks: # we have links to follow
+    if currentpagelinks:  # we have links to follow
         for linkstring in currentpagelinks:
             if not linkstring in alreadyvisited and domain in linkstring:
                 retrieveURL(linkstring)
@@ -37,8 +37,8 @@ my_parser.add_argument("-i", "--internal", help="only list internal links (to th
 my_parser.add_argument("-e", "--external", help="only list external links (to the TLD)", action="store_true")
 
 args = my_parser.parse_args()
-if not args.internal and not args.external: # in case the -i AND -e options are not specified
-    args.internal = True                    # we want them to be both true to show everything
+if not args.internal and not args.external:  # in case the -i AND -e options are not specified
+    args.internal = True                     # we want them to be both true to show everything
     args.external = True
 target = args.url
 ext = tldextract.extract(target)
