be2de29a8386a5c43df3df135fb2d56ba7960e59
From Surface <christophe.dehon@gmail.com>
2020-09-28 10:39:41 +0100

    Added recursion, link validation and tracking of links already visited

--- webparser.py
+++ webparser.py
@@ -2,13 +2,26 @@ import requests
 import bs4
 import argparse
 
-def retrieveURL(url):
-    print("#### Retrieving: "+ url)
-    site = requests.get(target)
-    # print(site.content)
+alreadyvisited = [] # list of urls that have already been crawled
+
+def retrieveURL(turl):
+    print("#### Retrieving: "+ turl)
+    alreadyvisited.append(turl) # keeping track of urls already visited
+    currentpagelinks = [] # list of links on the page being parsed
+    site = requests.get(turl)
     soup = bs4.BeautifulSoup(site.content, 'html.parser')
     for link in soup.find_all('a'):
-        print(link.get('href'))
+        linkstring = link.get('href')
+        if linkstring is not None:
+            if linkstring[0:4] == 'http': # ignoring anchors etc...
+                print(linkstring)
+                currentpagelinks.append(linkstring)
+        
+    if currentpagelinks: # we have links to follow
+        for linkstring in currentpagelinks:
+            if not linkstring in alreadyvisited:
+                retrieveURL(linkstring)
+
 
 my_parser = argparse.ArgumentParser(description='Crawl URL and list all <a> links found')
 my_parser.add_argument('url',
