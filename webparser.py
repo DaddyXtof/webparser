1c1a704a80a07fd89ee665462c9350959b0f60b6
From Surface <christophe.dehon@gmail.com>
2020-09-28 10:04:59 +0100

    Implemented argparse

--- webparser.py
+++ webparser.py
@@ -1,9 +1,17 @@
 import requests
 import bs4
+import argparse
 
-target = "http://onecloudstreet.com"
+my_parser = argparse.ArgumentParser(description='Crawl URL and list all <a> links found')
+my_parser.add_argument('url',
+                       metavar='url',
+                       type=str,
+                       help='the URL to crawl - for example: http://onecloudstreet.com')
+args = my_parser.parse_args()
+target = args.url
 site = requests.get(target)
-print(site.content)
+# print(site.content)
 soup = bs4.BeautifulSoup(site.content, 'html.parser')
 for link in soup.find_all('a'):
     print(link.get('href'))
+
